d290f1e7f037d9625771a24506a4aa240058a866
user1896 <abde.user15@gmail.com>
2024-05-22 02:00:39 +0200

    First routing solution that requires typing the URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Routes } from "react-router-dom";
-import GitNavbar from "./components/content/Git-doc/GitNavbar";
-import TopNavbar from "./components/top-navbar/TopNavbar";
+import { Routes, Route } from "react-router-dom";
 import GitDoc from "./components/content/Git-doc/GitDoc";
 import Chapter1Page from "./components/content/Git-doc/chapter-1/full-chapter/Page";
 import Chapter2Page from "./components/content/Git-doc/chapter-2/full-chapter/Page";
@@ -14,16 +12,17 @@ function App() {
   Check useEffect.
   */
 
-
   /* all accordions should share the same state, when one of them gets expanded, the other shrinks */
   return (
     <ToggleNavbarProvider>
       <div className="bg-zinc-800 text-white min-h-screen pt-12">
-        <GitDoc />
+        <Routes>
+          <Route exact path='/' element={<GitDoc />} />
+          <Route path='/1' element={<Chapter1Page />} />
+          <Route path='/2' element={<Chapter2Page />} />
+        </Routes>
       </div>
     </ToggleNavbarProvider>
-    /* create a single page that holds all pages of a single document in one web page, so we can search for 
-    a word with "ctr+f" */
   );
 }
 
